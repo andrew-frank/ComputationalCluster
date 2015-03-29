776f5db6548d9712e1f814ec0a9bb67038896cc4
Andrzej <andrewfrankowski@gmail.com>
2015-03-29 19:16:48 +0200

    Initial commit. Projects and tests referenced.

---
+++ ComputationalCluster/Connection/ICommunicationServer.cs
@@ -0,0 +1,13 @@
+﻿using ComputationalCluster.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputationalCluster.Connection {
+    interface ICommunicationServer 
+    {
+        
+    }
+}
---
+++ ComputationalClusterClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputationalClusterClient {
+    class Program {
+        static void Main(string[] args) 
+        {
+            // get config file from runtime args
+            // or if none provided, request config
+            // via console
+            setConfigFileAtRuntime(args);
+        }
+
+        protected static void setConfigFileAtRuntime(string[] args) {
+            string runtimeconfigfile;
+
+            if (args.Length == 0) {
+                Console.WriteLine("Please specify a config file:");
+                Console.Write("> "); // prompt
+                runtimeconfigfile = Console.ReadLine();
+            } else {
+                runtimeconfigfile = args[0];
+            }
+
+            
+        }
+
+
+    }
+
+
+
+}
