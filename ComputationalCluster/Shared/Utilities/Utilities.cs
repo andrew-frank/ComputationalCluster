a050c030f18ec1225116287760f140ea2162f082
Andrzej <andrewfrankowski@gmail.com>
2015-03-30 02:06:06 +0200

    XML parsing - searching for object type

--- ComputationalCluster/Shared/Utilities/Utilities.cs
+++ ComputationalCluster/Shared/Utilities/Utilities.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ComputationalCluster.Shared.Messages.DivideProblemNamespace;
+using ComputationalCluster.Shared.Messages.NoOperationNamespace;
+using ComputationalCluster.Shared.Messages.RegisterNamespace;
+using ComputationalCluster.Shared.Messages.RegisterResponseNamespace;
+using ComputationalCluster.Shared.Messages.SolutionsNamespace;
+using ComputationalCluster.Shared.Messages.SolvePartialProblemsNamespace;
+using ComputationalCluster.Shared.Messages.SolveRequestNamespace;
+using ComputationalCluster.Shared.Messages.SolveRequestResponseNamespace;
+using ComputationalCluster.Shared.Messages.StatusNamespace;
+
 
 namespace ComputationalCluster.Shared.Utilities {
     public static class Utilities 
@@ -24,16 +34,31 @@ namespace ComputationalCluster.Shared.Utilities {
         }
 
 
-        public static Object DeserializeXML(this string XMLString, Type type)
+        public static Object DeserializeXML(this string XMLString)
         {
-            System.Xml.Serialization.XmlSerializer oXmlSerializer = new System.Xml.Serialization.XmlSerializer(type); 
-            
-            //The StringReader will be the stream holder for the existing XML file 
-            Object YourClassObject = oXmlSerializer.Deserialize(new System.IO.StringReader(XMLString)); 
-            
-            //initially deserialized, the data is represented by an object without a defined type 
-            return YourClassObject;
+            List<Type> types = new List<Type>();
+            types.Add(typeof(DivideProblem));
+
+            System.Xml.Serialization.XmlSerializer oXmlSerializer;
+
+            foreach(Type type in types) {
+                try {
+                    oXmlSerializer = new System.Xml.Serialization.XmlSerializer(type);
+
+                    //The StringReader will be the stream holder for the existing XML file 
+                    Object YourClassObject = oXmlSerializer.Deserialize(new System.IO.StringReader(XMLString));
+
+                    //initially deserialized, the data is represented by an object without a defined type 
+                    return YourClassObject;
+
+                } catch (Exception e) {
+                    Console.WriteLine("XML parsing: Catching wrong type exception while searching for object. \n" + e.ToString());
+                }
+            }
+
+            return null;
         }
 
+        
     }
 }
