3b8ac3d81de6ea5bc3ca65259ee4add3b28141ee
frandrzej <andrewfrankowski@gmail.com>
2015-03-31 13:02:57 +0200

    RegisteredNodes improved

--- ComputationalCluster/Server/Server.cs
+++ ComputationalCluster/Server/Server.cs
@@ -32,25 +32,83 @@ namespace ComputationalCluster.Nodes
         public List<Node> ComputationalNodes { get { return _computationalNodes; } }
         public List<Node> BackupServers { get { return _backupServers; } }
 
-        public void RegisterClient()
-        {
 
+        public void RegisterClient(Node node)
+        {
+            this.Clients.Add(node);
         }
 
-        public void RegisterTaskManager()
+        public void RegisterTaskManager(Node node)
         {
-
+            this.TaskManagers.Add(node);
         }
 
-        public void RegisterComputationalNode()
+        public void RegisterComputationalNode(Node node)
         {
-
+            this.ComputationalNodes.Add(node);
         }
 
-        public void RegisterBackupServer()
+        public void RegisterBackupServer(Node node)
         {
-
+            this.BackupServers.Add(node);
         }
+
+        public bool DeregisterClient(Node node) 
+        {
+            int i = 0;
+            foreach (Node n in Clients) {
+                if (n.ID == node.ID) {
+                    Clients.RemoveAt(i);
+                    return true;
+                }
+
+                i++;
+            }
+            return false;
+        }
+        
+        public bool DeregisterBackupServer(Node node)
+        {
+            int i = 0;
+            foreach (Node n in BackupServers) {
+                if (n.ID == node.ID) {
+                    Clients.RemoveAt(i);
+                    return true;
+                }
+
+                i++;
+            }
+            return false;
+        }
+
+        public bool DeregisterComputationalNode(Node node)
+        {
+            int i = 0;
+            foreach (Node n in ComputationalNodes) {
+                if (n.ID == node.ID) {
+                    Clients.RemoveAt(i);
+                    return true;
+                }
+
+                i++;
+            }
+            return false;
+        }
+
+        public bool DeregisterTaskManagers(Node node)
+        {
+            int i = 0;
+            foreach (Node n in TaskManagers) {
+                if (n.ID == node.ID) {
+                    Clients.RemoveAt(i);
+                    return true;
+                }
+
+                i++;
+            }
+            return false;
+        }
+
     }
 
 
