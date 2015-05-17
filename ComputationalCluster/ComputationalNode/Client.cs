2e1d76b6622135ed14cae56d4ad8dfd052947d3b
Lino3D <kamiskijakub@gmail.com>
2015-05-17 21:03:22 +0200

    Okay classes to DVRP

---
+++ ComputationalCluster/ComputationalNode/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace ComputationalCluster.ComputationalNode
+{
+  public class Client
+    {
+      public Client(Point _location, double _time, double _unloadTime, int _size)
+      {
+          Location = _location;
+          Time = _time;
+          _unloadTime = unloadTime;
+          Size = _size;
+      }
+
+      Point Location { get; set; }
+      double Time;
+      double unloadTime;
+      int Size;
+     
+    }
+
+
+
+}
