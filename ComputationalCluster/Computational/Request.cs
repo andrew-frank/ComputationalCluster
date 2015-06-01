638ac7d3fbaff0674d6e1237a83757671094e44f
blathursby <blathursby@hotmail.com>
2015-06-01 18:04:29 +0200

    Added Class Request

---
+++ ComputationalCluster/Computational/Request.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComputationalCluster.Computational
+{
+    class Request
+    {
+        public int Id { get; set; }
+
+        public Point Location { get; set; }
+
+        public double Start { get; set; }
+
+        public double End { get; set; }
+
+        public double Unload { get; set; }
+
+        public double Size { get; set; }
+    }
+}
