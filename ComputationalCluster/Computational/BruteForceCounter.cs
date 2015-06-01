60aa6b427feb7176f5856fabc10326f587c88276
blathursby <blathursby@hotmail.com>
2015-06-01 18:14:58 +0200

    Added BruteForceCounter class

---
+++ ComputationalCluster/Computational/BruteForceCounter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputationalCluster.Computational
+{
+    class BruteForceCounter
+    {
+        static BruteForceCounter()
+        {
+            PossibleRequests = new List<Request>();
+        }
+
+        public static IList<Route> MinPath { get; set; }
+
+        public static double MinPathDistance { get; set; }
+
+        public static int MaximumOption { get; set; }
+
+        public static int CurrentOption { get; set; }
+
+        public static IList<Request> PossibleRequests { get; set; }
+    }
+}
